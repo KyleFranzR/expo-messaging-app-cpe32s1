1aaa98ed034941aebf7794823123d228e3c10020
Kyle-Franz-Rodriguez <qkfprodriguez@tip.edu.ph>
2026-04-09 19:21:24 +0800

    first update: HOA 12.1

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import Status from './components/Status';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Status/>
+
+      <View style={styles.content}>
+          <Text>content</Text>
+      </View>
+      
+      <View style={styles.toolbar}>
+        <Text>toolbar</Text>
+      </View>
+      
+      <View style={styles.inputMethodEditor}>
+        <Text>inputMethodEditor</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +25,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  content: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  inputMethodEditor: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  toolbar: {
+    borderTopWidth: 1,
+    borderTopColor: 'rgba(0,0,0,0.04)',
+    backgroundColor: 'white',
   },
 });
